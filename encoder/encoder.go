54d9f76c416fd805a7d45e1dacd26aad3b0f5dc9
Sam <earthstamper@gmail.com>
2020-09-04 04:25:21 +0200

    encoder parsing

--- encoder/encoder.go
+++ encoder/encoder.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"github.com/Spiritreader/avior-go/config"
+	"github.com/Spiritreader/avior-go/globalstate"
 	"github.com/Spiritreader/avior-go/media"
 	"github.com/kpango/glg"
 	"github.com/rs/xid"
@@ -25,6 +26,8 @@ type Stats struct {
 	ExitCode   int
 	OutputPath string
 }
+
+var state *globalstate.Data = globalstate.Instance()
 
 func Encode(file media.File, start, duration int, overwrite bool) (Stats, error) {
 	cfg := config.Instance()
@@ -72,9 +75,9 @@ func Encode(file media.File, start, duration int, overwrite bool) (Stats, error)
 		return Stats{false, -1, -1337, ""}, err
 	}
 	scanner := bufio.NewScanner(multiReader)
-	scanner.Split(ScanLinesWithCR)
+	scanner.Split(ScanLinesSTDOUT)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		parseOut(scanner.Text())
 	}
 	if err := cmd.Wait(); err != nil {
 		_ = glg.Errorf("ffmpeg error: %s", err)
@@ -87,14 +90,15 @@ func Encode(file media.File, start, duration int, overwrite bool) (Stats, error)
 	return Stats{true, encTime, exitCode, outPath}, nil
 }
 
-func ScanLinesWithCR(data []byte, atEOF bool) (advance int, token []byte, err error) {
+func ScanLinesSTDOUT(data []byte, atEOF bool) (advance int, token []byte, err error) {
     if atEOF && len(data) == 0 {
         return 0, nil, nil
 	}
 	if i := bytes.IndexByte(data, '\n'); i >= 0 {
+		// We have a full newline-terminated line.
 		return i + 1, data[0:i], nil
 	} else if i := bytes.IndexByte(data, '\r'); i >= 0 {
-        // We have a full newline-terminated line.
+        // We have a refresh that needs to be printed
         return i + 1, data[0:i], nil
     }
     // If we're at EOF, we have a final, non-terminated line. Return it.
@@ -103,4 +107,84 @@ func ScanLinesWithCR(data []byte, atEOF bool) (advance int, token []byte, err er
     }
     // Request more data.
     return 0, nil, nil
+}
+
+func parseOut(line string) {
+	durationToken := "Duration:"
+	frameToken := "frame="
+	fpsToken := "fps="
+	qToken := "q="
+	sizeToken := "size="
+	timeToken := "time="
+	bitrateToken := "bitrate="
+	dupToken := "dup="
+	dropToken := "drop="
+	speedToken := "speed="
+
+	state.Encoder.LineOut = append(state.Encoder.LineOut, line)
+	if strings.Contains(line, durationToken) {
+		keyIdx := strings.Index(line, durationToken) + len(durationToken)
+		timeIdx := strings.Index(line, ".")
+		state.Encoder.Duration, _ = time.Parse("15:04:05", strings.Trim(line[keyIdx:timeIdx], " "))
+	// safe enough for now
+	} else if strings.Contains(line, frameToken) {
+		// ffmpeg out looks like this: 
+		// frame=  272 fps=271 q=15.0 size=     512kB time=00:00:11.07 bitrate= 378.8kbits/s dup=0 drop=269 speed=  11x
+		
+		next := strings.Split(line, frameToken)
+		// next[0] empty 
+
+
+		next = strings.Split(next[1], fpsToken)
+		// next[0] now holds frame value
+		frameParse, _ := strconv.ParseInt(strings.Trim(next[0], " "), 10, 32)
+		state.Encoder.Frame = int(frameParse)
+
+
+		next = strings.Split(next[1], qToken)
+		// next[0] now holds fps value
+		state.Encoder.Fps, _ = strconv.ParseFloat(strings.Trim(next[0], " "), 64)
+
+
+		next = strings.Split(next[1], sizeToken)
+		// next[0] now holds q value
+		state.Encoder.Q, _ = strconv.ParseFloat(strings.Trim(next[0], " "), 64)
+
+
+		next = strings.Split(next[1], timeToken)
+		// next[0] now holds size value
+		state.Encoder.Size = strings.Trim(next[0], " ")
+
+
+		next = strings.Split(next[1], bitrateToken)
+		// next[0] now time value
+		state.Encoder.Position, _ = time.Parse("15:04:05", strings.Trim(next[0][:strings.Index(next[0], ".")], " "))
+
+
+		next = strings.Split(next[1], dupToken)
+		// next[0] now holds bitrate value
+		state.Encoder.Bitrate = strings.Trim(next[0], " ")
+
+
+		next = strings.Split(next[1], dropToken)
+		// next[0] now holds dup value
+		dupParse, _ := strconv.ParseInt(strings.Trim(next[0], " "), 10, 32)
+		state.Encoder.Dup = int(dupParse)
+
+
+		next = strings.Split(next[1], speedToken)
+		// next[1] now holds speed value
+		state.Encoder.Speed, _ = strconv.ParseFloat(strings.Trim(next[1][:strings.Index(next[1], "x")], " "), 64)
+	}
+
+	fmt.Printf("Duration: %s\n", state.Encoder.Duration)
+	fmt.Printf("Frame: %d\n", state.Encoder.Frame)
+	fmt.Printf("Fps: %f\n", state.Encoder.Fps)
+	fmt.Printf("Q: %f\n", state.Encoder.Q)
+	fmt.Printf("Size: %s\n", state.Encoder.Size)
+	fmt.Printf("Position: %s\n", state.Encoder.Position)
+	fmt.Printf("Bitrate: %s\n", state.Encoder.Bitrate)
+	fmt.Printf("Dup: %d\n", state.Encoder.Dup)
+	fmt.Printf("Drop: %d\n", state.Encoder.Drop)
+	fmt.Printf("Speed: %f\n", state.Encoder.Speed)
 }
--- globalstate/globalstate.go
+++ globalstate/globalstate.go
@@ -1,1 +1,37 @@
 package globalstate
+
+import (
+	"sync"
+	"time"
+)
+
+var once sync.Once
+var instance *Data
+
+// Instance retrieves the current configuration file instance
+//
+// Creates a new one if it doesn't exist
+func Instance() *Data {
+	once.Do(func() {
+		instance = new(Data)
+	})
+	return instance
+}
+
+type Data struct {
+	Encoder Encoder
+}
+
+type Encoder struct {
+	LineOut  []string
+	Duration time.Time
+	Frame    int
+	Fps      float64
+	Q        float64
+	Size     string
+	Position time.Time
+	Bitrate  string
+	Dup      int
+	Drop     int
+	Speed    float64
+}
